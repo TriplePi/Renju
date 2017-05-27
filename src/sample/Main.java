cde2eb951778fcfdd8fc2b3c526645bf05565f1d
LauraEchis <laura.echis@gmail.com>
2017-05-27 15:45:06 +0400

    теперь банановые (ровные)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Renju");
-        primaryStage.setScene(new Scene(root, 1356, 910));
+        primaryStage.setScene(new Scene(root, 1355, 903));
         primaryStage.show();
     }
 
